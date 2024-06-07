5992bb4c8983403b8cebc63fa77b38ccaf76975d
soaresdutra97 <soaresdutra97@gmail.com>
2024-06-07 13:53:26 -0300

    Department Entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,33 +6,15 @@ import java.sql.SQLException;
 
 import db.DB;
 import db.DbIntegrityException;
+import entities.Department;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		Connection conn = null;
-		PreparedStatement st = null;
-		try {
-			conn = DB.getConnection();
-	
-			st = conn.prepareStatement(
-					"DELETE FROM department "
-					+ "WHERE "
-					+ "Id = ?");
+		Department obj = new Department(1, "Books");
+		System.out.println(obj);
 
-			st.setInt(1, 5);
-			
-			int rowsAffected = st.executeUpdate();
-			
-			System.out.println("Done! Rows affected: " + rowsAffected);
-		}
-		catch (SQLException e) {
-			throw new DbIntegrityException(e.getMessage());
-		} 
-		finally {
-			DB.closeStatement(st);
-			DB.closeConnection();
-		}
+
 	}
 }
