bfe73df5b96ddd7490c306ebbd47a06a1735c17b
soaresdutra97 <soaresdutra97@gmail.com>
2024-06-07 14:08:51 -0300

    SellerDAO e DaoFactory implemented

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,6 +9,8 @@ import db.DB;
 import db.DbIntegrityException;
 import entities.Department;
 import entities.Seller;
+import model.dao.DaoFactory;
+import model.dao.SellerDAO;
 
 public class Program {
 
@@ -16,6 +18,9 @@ public class Program {
 
 		Department obj = new Department(1, "Books");
 		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+
+		SellerDAO sellerDAO = DaoFactory.createSellerDao();
+
 		System.out.println(seller);
 
 	}
---
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -0,0 +1,34 @@
+package model.dao.impl;
+
+import entities.Seller;
+import model.dao.SellerDAO;
+
+import java.util.List;
+
+public class SellerDaoJDBC implements SellerDAO {
+
+    @Override
+    public void insert(Seller obj) {
+
+    }
+
+    @Override
+    public void update(Seller obj) {
+
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+
+    }
+
+    @Override
+    public Seller findById(Integer Id) {
+        return null;
+    }
+
+    @Override
+    public List<Seller> findAll() {
+        return List.of();
+    }
+}
