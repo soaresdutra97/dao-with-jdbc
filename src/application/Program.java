10198ee18089fc917ae472553485e8e9853ffefc
soaresdutra97 <soaresdutra97@gmail.com>
2024-06-07 13:58:46 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,18 +3,20 @@ package application;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.Date;
 
 import db.DB;
 import db.DbIntegrityException;
 import entities.Department;
+import entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
 		Department obj = new Department(1, "Books");
-		System.out.println(obj);
-
+		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+		System.out.println(seller);
 
 	}
 }
